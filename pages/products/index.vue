e47e7df5cfcd6ff85cd82198b2aaf9c76bf67b27
Joseph42A <82284166+Joseph42A@users.noreply.github.com>
2023-07-01 20:27:08 +0300

    Nuxt Store

---
+++ pages/products/index.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <!-- <h1>Products</h1> -->
+    <div class="grid grid-cols-4 gap-5">
+      <div v-for="product in products" :key="product.id">
+        <ProductCard :product="product" />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup>
+definePageMeta({
+  layout: "products",
+});
+
+const {
+  data: { _rawValue: products },
+} = await useFetch("https://fakestoreapi.com/products");
+
+useHead({
+  title: "Merch",
+  meta: [{ name: "description", content: "Merch in Joso store" }],
+});
+</script>
+
+<style scoped></style>
